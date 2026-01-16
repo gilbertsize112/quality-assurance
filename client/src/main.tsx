c01c8c6518613b1e4802c043d35feb67f8b99ef3
gilbertsize112 <jamesize81@gmail.com>
2026-01-16 10:06:52 +0100

    Updated files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,3 +8,16 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => {
+        console.log('NDDC-QMP Service Worker registered successfully:', reg.scope);
+      })
+      .catch(err => {
+        console.log('NDDC-QMP Service Worker registration failed:', err);
+      });
+  });
+}
