a3d4e7f4bce76666c16a73363c1d5716f66d0108
gilbertsize112 <jamesize81@gmail.com>
2026-01-16 10:55:19 +0100

    Updated files

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,7 +1,41 @@
+const CACHE_NAME = 'nddc-qmp-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/manifest.json'
+];
+
+// Install stage: Saves the logo and main files to the phone
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
+// Activate stage: Cleans up old versions of the app
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch stage: Makes the app load faster and work offline
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
